5c60a6a39e7118bcd40b382f30a315e92eae56ad
Pop0097 <dhruvrawat09@gmail.com>
2020-04-23 22:15:45 -0400

    Update
    I improved the readability of the code so it is easier to understand.

--- Main.java
+++ Main.java
@@ -1,160 +1,89 @@
-//input your own rectangle dimensions (the main method calls the game like features)
-
-
 import java.util.Scanner;
 
 public class Main {
 
+	static Scanner input = new Scanner(System.in);
+
+
 	public static void main(String args[]) {
-		
-		Scanner input = new Scanner(System.in);
-		
 		int left, bottom, width, height, area, perimeter;
 		boolean contain;
 
-		System.out.println("Write the characteristics of the first rectangle (bottom left x coordinate, bottom left y coordinate, width, height");
+		//Gets the properties of Rectangle 1 from the user
+		System.out.println("Enter the properties of Rectangle 1:");
+		System.out.print("x-coordinate of the bottom left corner: ");
 		left = Integer.valueOf(inputs());
+		System.out.print("y-coordinate of the bottom left corner: ");
 		bottom = Integer.valueOf(inputs());
+		System.out.print("Width: ");
 		width = Integer.valueOf(inputs());
+		System.out.print("Height: ");
 		height = Integer.valueOf(inputs());
-	
-		Rectangle r1 = new Rectangle(left, bottom, width, height);
-		
-		System.out.println(r1);
-		
-		area = r1.area();
-		perimeter = r1.perimeter();
-		
-		//rectangle 2
-		
+
+		Rectangle r1 = new Rectangle(left, bottom, width, height); //creates a Rectangle object for Rectangle 1
+
 		System.out.println();
-		
-		System.out.println("Write the characteristics of the second rectangle (bottom left x coordinate, bottom left y coordinate, width, height");
+
+		//Gets the properties of Rectangle 2 from the user
+		System.out.println("Enter the properties of Rectangle 2:");
+		System.out.print("x-coordinate of the bottom left corner: ");
 		left = Integer.valueOf(inputs());
+		System.out.print("y-coordinate of the bottom left corner: ");
 		bottom = Integer.valueOf(inputs());
+		System.out.print("Width: ");
 		width = Integer.valueOf(inputs());
+		System.out.print("Height: ");
 		height = Integer.valueOf(inputs());
-		
-		/*
-		left = -4;
-		bottom = -2;
-		width = 3;
-		height = 2;
-		*/
-		
-		Rectangle r2 = new Rectangle(left, bottom, width, height);
-		
-		System.out.println(r2);
-		
+
+		Rectangle r2 = new Rectangle(left, bottom, width, height); //creates a Rectangle object for Rectangle 2
+
+		//outputs the total perimeter of Rectangle 1 and 2
 		System.out.println();
-		
-		contain =  r1.contains(r2);
-		
+		System.out.println("The total perimeter of the polygon made by Rectangle 1 and 2 is " + r1.totalPerimeter(r1, r2));
+
+		System.out.println();
+
+		//Creates Rectangle 3, which is the rectangle formed from the intersection of Rectangle 1 and 2
 		Rectangle r3 = new Rectangle();
-		
-		r3 = r3.intersection(r1, r2);
-		
-		//for you to refer to when checking r3 in the game like features
-		System.out.println("r3 --> " + r3);
-		
-		perimeter = r1.totalPerimeter(r1, r2);
-		
-		System.out.println();
-		
+		r3 = r3.containedRectangle(r1, r2);
+
+		//prints all the three rectangles
 		r1.printRectangles(r2, r3);
-		
+
 	}
-	
-	public static String inputs() {
-		
-		Scanner input = new Scanner(System.in);
-		
+
+	public static String inputs() { //this method collects the input from the user
 		String userInput = "";
-		
-		boolean done1 = false;
-		
-		while(done1 == false) {
-			
-			userInput = input.next();
-			done1 = isValid(userInput);
-			//System.out.println(userInput);
-			
+		boolean done = false;
+
+		while(done == false) {
+			userInput = input.next(); //collects input as a string so the program does not crash if the user does not input a number
+			done = isValid(userInput);
 		}
-		
-		
 		return userInput;
 	}
-	
-	public static boolean isValid(String input){
-		
-		boolean result = false;
+
+	public static boolean isValid(String input){ //this method checks if the input is valid
+		boolean result = true;
 		int length = input.length();
 		char firstChar = input.charAt(0);
 		char secondChar;
-		boolean invalid = false;
 		int ACSII1 = (int) firstChar;
 		int ACSII2 = 0;
-		
-		//delete later
-		//System.out.println("Length: " + length);
-		
-		if(ACSII1 == 45) {
-			
-			for(int i = 1; i < length; i++) {
-				
+
+		if(ACSII1 == 45 || (ACSII1 >= 48 && ACSII1 <= 57)) { //If the first character is a "-" (for negative numbers) or a number (for positive numbers)
+			for(int i = 1; i < length; i++) { //checks if every character is a number
 				secondChar = input.charAt(i);
-				//delete later
-				//System.out.println("Char2: " + secondChar);
-				
 				ACSII2 = (int) secondChar;
-				if(ACSII2 < 48 || ACSII2 > 57) {
-					
-					invalid = true;
-					
+				if(ACSII2 < 48 || ACSII2 > 57) { //if at least one character is not a number
+					result = false;
 				}
-			}
-		}
-		else if(ACSII1 >= 49 && ACSII1 <= 57) {
-			
-			for(int i = 0; i < length; i++) {
-				
-				secondChar = input.charAt(i);
-				//delete later
-				//System.out.println("Char1: " + secondChar);
-				
-				ACSII2 = (int) secondChar;
-				if(ACSII2 < 48 || ACSII2 > 57) {
-					
-					invalid = true;
-					
-				}
-			}
-		}
-		else if(ACSII1 == 48) {
-			
-			if(length != 1) {
-				
-				invalid = true;
-				
 			}
 		}
 		else {
-			
-			//delete later
 			System.out.println("Invalid input. Ensure input is an integer");
-			invalid = true;
-			
+			result = false;
 		}
-		
-		if(invalid == false) {
-			
-			result = true;
-			
-		}
-		
-		//delete later
-		//System.out.println(result);
-		
 		return result;
 	}
 }
--- Rectangle.java
+++ Rectangle.java
@@ -1,344 +1,202 @@
-
-
-//the program also prints each rectangle individually. Call the method Rectangle.printRectangles(Rectangle, Rectangle) to print all three rectangles. 
-
-
 class Rectangle
 {
 	private int x, y, w, h;
-	
-	//2
+
+	//default constructor for the Rectangle object
 	public Rectangle() {
-		
 		this.x = 0;
 		this.y = 0;
 		this.w = 0;
 		this.h = 0;
-		
 	}
-	
-	//3
+
+	//constructor for the Rectangle object
 	public Rectangle(int left, int bottom, int width, int height) {
-		
-		set(left, bottom, width, height);
-		
+		set(left, bottom, width, height); //sets values for the rectangle
 	}
-	
-	//4
+
+	//checks if the inputs are valid and modifies invalid inputs
 	public void set(int xx, int yy, int ww, int hh) {
-		
 		this.x = xx;
 		this.y = yy;
 		this.w = ww;
 		this.h = hh;
-		
-		//if dimensions are negative, set value to 0
+
+		//if dimensions are negative, set value to 0 and the rectangle will be treated as a line
 		if(ww < 0) {
-			
 			this.w = 0;
-			
 		}
-		
 		if(hh < 0) {
-			
 			this.h = 0;
-			
 		}
 	}
-	
-	//5
+
 	public String toString() {
-		
-		return "base: (" + this.x + "," + this.y + ") w:" + this.w + " h:" + this.h;
-		
+		return "Bottom-left corner coordinates: (" + this.x + "," + this.y + ")\nDimensions: " + this.w + " by " + this.h + "\nPerimeter: " + this.perimeter() + "\nArea: " + this.area();
 	}
-	
-	//6
+
+	//calculates the area of the object
 	public int area() {
-		
-		int area = 0;
-		
-		area = this.w * this.h;
-				
-		return area;
-		
+		return this.w * this.h;
+
 	}
-	
-	//7
+
+	//calculates the perimeter of the object
 	public int perimeter() {
-		
 		int perimeter = 0;
-		
+		//the first two statements check if the object is a line.
 		if(this.w == 0) {
-			
 			perimeter = this.h;
-			
 		}
 		else if(this.h == 0) {
-			
 			perimeter = this.w;
-			
 		}
 		else {
-			
 			perimeter = 2*this.w + 2*this.h;
-			
 		}
-				
 		return perimeter;
-	
 	}
-	
-	//8
+
+	//this function checks if the r2 is fully contained in the rectangle whose instance is passed
 	public boolean contains(Rectangle r2) {
-		
-		boolean result = false;
-		
+		boolean contains = false;
+
 		//the top right coordinate for Rectangle 1
 		int r1x2 = this.x+this.w;
 		int r1y2 = this.y+this.h;
-		
+
 		//the top right coordinate for Rectangle 2 (this one is being checked to see if it is inscribed in triangle 1)
 		int r2x2 = r2.x+r2.w;
 		int r2y2 = r2.y+r2.h;
-		
+
 		if(this.x <= r2.x && this.y <= r2.y && r1x2 >= r2x2 && r1y2 >= r2y2) { //checks if all coordinates of the second rectangle fall within those of the first rectangle
-				
-			result = true;
-				
+			contains = true;
 		}
-		
-		return result;
-		
+		return contains;
 	}
 
-	//9
-	public static Rectangle intersection(Rectangle r1, Rectangle r2){
+	//Finds the rectangle that is contained by both r1 and r2
+	public static Rectangle containedRectangle(Rectangle r1, Rectangle r2){
 		Rectangle r3 = new Rectangle();
-		
-		if(((r1.x + r1.w) < r2.x) || ((r2.x +r2.w) < r1.x) || ((r1.y + r1.h) < r2.y) || ((r2.y + r2.h)< r1.y)){ //checks to see if the rectangles do not intersect at all
-			
+		if(((r1.x + r1.w) < r2.x) || ((r2.x +r2.w) < r1.x) || ((r1.y + r1.h) < r2.y) || ((r2.y + r2.h)< r1.y)){ //checks to see if the rectangles do not intersect at all			return r3;
 			return r3;
-			
-		} 
+		}
 		else{ //if it is concluded that they do intersect, coordinates are determined.
-		
 			r3.x = Math.max(r1.x, r2.x);
 			r3.y = Math.max(r1.y, r2.y);
 			r3.w = Math.min((r1.x + r1.w), (r2.x + r2.w)) - r3.x;
-			r3.h = Math.min((r1.y + r1.h), (r2.y + r2.h)) - r3.y;  
-			
+			r3.h = Math.min((r1.y + r1.h), (r2.y + r2.h)) - r3.y;
 			return r3;
-		 
-		} 
+		}
 	}
-	
-	//10
+
+	//Calculates the perimeter of the polygon made by r1 and r2. If r1 and r2 do not overlapp, the sum of their perimeters is returned
 	public static int totalPerimeter(Rectangle r1, Rectangle r2) {
-		
-		int perimeter = 0;
-		
-		//temporary placeholder rectangles that will be used in operations
-		Rectangle a = new Rectangle(); 
-		Rectangle b = new Rectangle();//rectangle that is contained
-		
-		int r1p, r2p, r3p, subtract = 0;
-		
-	
+		int totalPerimeter = 0;
+
+		int r1Perimeter, r2Perimeter, r3Perimeter, subtract = 0;
+
 		//checks to see if the rectangles are contained and there is an edge that is overlapping (this case will change how the perimeter is calculated)
 		boolean contain1 = r1.contains(r2);
 		boolean contain2 = r2.contains(r1);
-		
-		Rectangle r3 = new Rectangle(); 
-		r3 = r3.intersection(r1, r2); //this is the rectangle formed from the overlapping area of r1 and r2
-		
-		//calculates perimeter of all rectangles 
-		r1p = r1.perimeter();
-		r2p = r2.perimeter();
-		r3p = r3.perimeter();
-		
+
+		Rectangle r3 = new Rectangle();
+		r3 = r3.containedRectangle(r1, r2); //this is the rectangle formed from the overlapping area of r1 and r2
+
+		//calculates perimeter of all rectangles
+		r1Perimeter = r1.perimeter();
+		r2Perimeter = r2.perimeter();
+		r3Perimeter = r3.perimeter();
+
 		//the top right coordinate for rectangle 1
 		int r1x2 = r1.x+r1.w;
 		int r1y2 = r1.y+r1.h;
-		
+
 		//the top right coordinate for rectangle 2 (this one is being checked to see if it is inscribed in triangle 1)
 		int r2x2 = r2.x+r2.w;
 		int r2y2 = r2.y+r2.h;
-				
-			
+
+
 		if(contain1 == true && contain2 == false) { //if r2 is fully contained in r1
-				
-			perimeter = r1p;
-				
+			totalPerimeter = r1Perimeter;
 		}
 		else if(contain2 == true && contain1 == false) {//if r1 is fully contained in r2
-				
-			perimeter = r2p;
-				
+			totalPerimeter = r2Perimeter;
 		}
-		
-		else{//if rectangles are not contained in any way or if rectangles do not overlap 
-				
+		else{//if rectangles are not contained in any way or if rectangles do not overlap
 			if((r1.h  == 0 || r2.h == 0) || (r1.w == 0 || r2.w == 0) ) {
-				
 				subtract = 0;
-				
 			}
 			else {
-				
 				if(r1.x == r2x2 || r1x2 == r2.x) {
-					
 					subtract += r3.h;
-					
 				}
 				if(r1.y == r2y2 || r1y2 == r2.y) {
-					
 					subtract += r3.w;
-					
 				}
 			}
-			
-			
-			perimeter = r1p + r2p - r3p - subtract;
-				
+			totalPerimeter = r1Perimeter + r2Perimeter - r3Perimeter - subtract;
+
 		}
-		
-		
-		return perimeter;
-	
+		return totalPerimeter;
 	}
-	
-	public boolean trueContain(Rectangle r2) {
-		
-		boolean result = false;
-		
-		//the top right coordinate for rectangle 1
-		int r1x2 = this.x+this.w;
-		int r1y2 = this.y+this.h;
-		
-		//the top right coordinate for rectangle 2 (this one is being checked to see if it is inscribed in triangle 1)
-		int r2x2 = r2.x+r2.w;
-		int r2y2 = r2.y+r2.h;
-		
-		if(this.x < r2.x && this.y < r2.y && r1x2 > r2x2 && r1y2 > r2y2) {
-			result = true;
-		}
-		
-		//System.out.println("true contain: " + result);
-		return result;
-		
-	}
-	
-	
-	//Game-like Features:
-	public void printRectangles(Rectangle r2, Rectangle r3) { // make this method be called from the total perimeter function
-			
-		int[] x = new int[3];
-			x[0] = this.x;
-			x[1] = r2.x;
-			x[2] = r3.x;
-		
-		int[] y = new int[3];
-			y[0] = this.y;
-			y[1] = r2.y;
-			y[2] = r3.y;
-			
-		int[] width = new int[3];
-			width[0] = this.w;
-			width[1] = r2.w;
-			width[2] = r3.w;
-		
-		int[] height = new int[3];
-			height[0] = this.h;
-			height[1] = r2.h;
-			height[2] = r3.h;
-			
-		int[]perimeter = new int[3];
-			perimeter[0] = this.perimeter();
-			perimeter[1] = r2.perimeter();
-			perimeter[2] = r3.perimeter();
-				
-		int[] area = new int[3];
-			area[0] = this.area();
-			area[1] = r2.area();
-			area[2] = r3.area();
-		
-		int totalP = this.totalPerimeter(this, r2);
-		
-		for(int i = 0; i < 3; i++){
-			
-			//if the rectangle has a side length of 0, it will not be printed using 'x', instead text will describe its properties
-			
+
+	//prints the rectangles using texts (one 'x' represents one unit)
+	public void printRectangles(Rectangle r2, Rectangle r3) {
+		int[] x = {this.x, r2.x, r3.x};
+		int[] y = {this.y, r2.y, r3.y};
+		int[] width = {this.w, r2.w, r3.w};
+		int[] height = {this.h, r2.h, r3.h};
+		Rectangle[] rectangles = {this, r2, r3};
+		boolean Rectangle3Exists = true;
+
+		for(int i = 0; i < 3; i++){ //this for loop prints out all three rectangles
+			//if the object has a side length of 0, it will not be printed using 'x'. Instead, text will describe its properties
 			System.out.println("Rectangle " + (i + 1) + ":");
 			System.out.println();
-			
-			if(height[i] == 0 && width[i] == 0) { //if both sides is 0
-				
-				if(i == 2 && ((this.x + this.w) < r2.x) || ((r2.x +r2.w) < this.x) || ((this.y + this.h) < r2.y) || ((r2.y + r2.h)< this.y) ) { //if r1 and r2 do not overlap and rectangle 3 is being observed
-					
-					System.out.println("Rectangle 3 does not exist because Rectangles 1 and 2 do not overlap");
-					
-				}
-				else { //present rectangle as a point
-					
-					System.out.println("Rectangle is just a point at (" + x[i] + "," + y[i] + ")");
-					
-				}
-				
-			}
-			else if(height[i] == 0 || width[i] == 0) { //if only one side is 0
-				
-				if(height[i] == 0) { 
-					
-					System.out.println("Rectangle is just a line that starts (left coordinate) at (" + x[i] + "," + y[i] + ") with a width of " + width[i]);
 
+			if(height[i] == 0 && width[i] == 0) { //if both sides have a magnitude of 0
+				if(i == 2 && ((this.x + this.w) < r2.x) || ((r2.x +r2.w) < this.x) || ((this.y + this.h) < r2.y) || ((r2.y + r2.h)< this.y) ) { //if r1 and r2 do not intersect
+					System.out.println("Rectangle 3 does not exist because Rectangles 1 and 2 do not intersect");
+					Rectangle3Exists = false;
 				}
-				else {
-					
-					System.out.println("Rectangle is just a line that starts (bottom coordinate) at (" + x[i] + "," + y[i] + ") with a height of " + height[i]);
-					
+				else { //presents object as a point
+					System.out.println("The rectangle is a point at (" + x[i] + "," + y[i] + ")");
 				}
-				
 			}
-			else { //if both sides are not 0
-				
-				for(int j = 0; j < height[i]; j++) { 
-					
-					if(j == 0 || j == height[i] - 1) { //if top or bottom, all rows have 'x'
-						
-						for(int k = 0; k < width[i]; k++) { 
-							
+			else if(height[i] == 0 || width[i] == 0) { //if only one side has a magnitude of 0
+				if(height[i] == 0) {  //horizontal line
+					System.out.println("The rectangle is a line that has its left-most coordinate (" + x[i] + "," + y[i] + ") with a length of " + width[i]);
+				}
+				else {	//vertical line
+					System.out.println("The rectangle is a line that has its bottom-most coordinate (" + x[i] + "," + y[i] + ") with a length of " + height[i]);
+				}
+			}
+			else { //if both sides have a magnitude greater than 0
+				for(int j = 0; j < height[i]; j++) {
+					if(j == 0 || j == height[i] - 1) { //if the program is printing the bottom or top most row
+						for(int k = 0; k < width[i]; k++) {
 							System.out.print("x");
-							
 						}
-						
 						System.out.println();
 					}
-					else { //else only extremes have 'x'
-						
+					else {
 						System.out.print("x");
-						
 						for(int l = 0; l < width[i]-2; l++) {
-							
 							System.out.print(" ");
-							
 						}
 						if(width[i] > 1) {
-							
 							System.out.print("x");
-							
 						}
-						
 						System.out.println();
-						
 					}
 				}
 			}
-			
 			System.out.println();
-			System.out.println("Width = " + width[i] + "\nHeight = " + height[i] + "\nPerimeter = " + perimeter[i] + "\nArea = " + area[i]); //prints characteristics of rectangles
+			if(!(i == 2 && !Rectangle3Exists)) { //does not print the Rectangle 3 if it is not defined
+				System.out.println(rectangles[i]); 
+			}
 			System.out.println();
-			
 		}
 	}
 }
